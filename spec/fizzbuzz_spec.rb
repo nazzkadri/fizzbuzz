3f8b851da54f4b11983eeb4f73007bf9987ca222
EC2 Default User <ec2-user@ip-172-31-19-13.us-east-2.compute.internal>
2018-08-20 13:46:02 +0000

    stage3tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -9,4 +9,19 @@ describe 'fizzbuzz' do
     it 'requires "fizzbuzz" when passed 15' do
         expect(fizzbuzz(15)).to eq 'fizzbuzz'
     end
+    it 'requires "fizz" when passed 9' do
+      expect(fizzbuzz(9)).to eq 'fizz'
+    end 
+    it 'requires "fizz" when passed 10' do
+      expect(fizzbuzz(10)).to eq 'buzz'
+    end
+    it 'requires nil when passed "xxx"' do
+        expect(fizzbuzz("xxx")).to eq nil
+    end
+    it 'requires 2 when passed 2' do
+        expect(fizzbuzz(2)).to eq 2
+    end
+    it 'requires 2.12 when passed 2.12' do
+        expect(fizzbuzz(2.12)).to eq 2.12
+    end
 end
